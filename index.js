cbf9c2d8f7bc448f960611fce1a0071ee5625e19
brianspurling <brian.spurling@gmail.com>
2016-11-29 23:41:37 +0000

    Tweaked error message for direct access

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var api = new ParseServer({
   appId:       process.env.APP_ID, // The unique ID used by the apps' Parse SDK to access this Parse Server
   fileKey:     process.env.FILE_ID, // Used by Parse Server to access files still stored on parse.com
   masterKey:   process.env.MASTER_KEY, // The Parse master key here.
-  serverURL:   process.env.SERVER_URL,  // The location to host this Parse Server. 
+  serverURL:   process.env.SERVER_URL,  // The location to host this Parse Server.
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -31,7 +31,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('The Balu Parse Server does not host any content directly. Try accessing one of the Balu apps instead.');
+  res.status(200).send('The Balu Parse Server is up and running, however it does not host any content directly. Try accessing one of the Balu apps instead.');
 });
 
 var port = process.env.PORT || 1337;
