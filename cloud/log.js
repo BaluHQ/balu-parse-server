c81f486723540135c048054f8dbaae28d3449b0b
brianspurling <brian.spurling@gmail.com>
2016-12-23 20:29:47 +0000

    Only return logs if logging level is turned on

--- cloud/log.js
+++ cloud/log.js
@@ -64,14 +64,28 @@ module.exports = {
  */
 function l(pvScriptName,pvFunctionName,pvMessage,pvLevel){
 
+    // This function can be passed a pre-formatted log string.
+    // In this case, just console.log it, without the preceeding or trailing carriage returns
+    if(typeof pvFunctionName === 'undefined' &&
+       typeof pvMessage === 'undefined' &&
+       typeof pvLevel === 'undefined') {
+        console.log(pvScriptName.substring(1,pvScriptName.length));
+        return '\n' + pvScriptName.substring(1,pvScriptName.length);
+    }
+
     // Build up our log text string
     var lvMaxAppNameLength = 22;
     var lvPadding = '                      '.substring(0,lvMaxAppNameLength - gvAppName.length + 1);
-    var lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+    var lvLogText = '';
+
     switch(pvLevel) {
 
         case 'ERROR':
-            if (gvLogErrors) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogErrors) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case 'PROCS':
@@ -79,53 +93,82 @@ function l(pvScriptName,pvFunctionName,pvMessage,pvLevel){
             // track (at the least) the start of every function, as well
             // as other key points
             // On by default
-            if (gvLogProcs)  console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogProcs) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case 'ROUTE':
             // Similar to PROCS, but for the web server routes
             // On by default
-            if (gvLogRoutes)  console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogRoutes) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case ' INFO':
             // Additional to PROCS, these don't just track process, they
             // record information as well. Similar to DEBUG.
             // Off by default
-            if (gvLogInfos) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogInfos) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case 'DEBUG':
             // Useful log points for debugging
             // Off by default
-            if (gvLogDebugs) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogDebugs) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case 'INITS':
             // Rather than putting PROCS in init functions (which always fire
             // and, once the app is working reliably, aren't particularly interesting)
             // Off by default
-            if (gvLogInits) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogInits) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case 'LSTNR':
             // Rather than putting PROCS in listeners (which can fire
             // continually in some scenarios), use LSTNR and keep them ...
             // Off by default
-            if (gvLogLstnrs) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogLstnrs) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         case ' TEMP':
             // What it says on the tin. These should not stay in the code for long
             // On by default
-            if (gvLogTemps) console.log(pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            if (gvLogTemps) {
+                lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + pvLevel + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+                console.log(lvLogText);
+                lvLogText = '\n' + lvLogText;
+            }
         break;
 
         default:
-            lvLogText = 'UNKNOWN LOG TYPE' + ' >>> ' + lvLogText;
-            console.log('UNKNN' + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage);
+            lvLogText = gvAppName.substring(0,lvMaxAppNameLength) + lvPadding + '| ' + 'UNKWN' + ': ' + pvScriptName + '.' + pvFunctionName + ': ' + pvMessage;
+            console.log(lvLogText);
+            lvLogText = '\n' + lvLogText;
     }
-    return '\n' + lvLogText;
+    return lvLogText; // Set to '' if logging is off for the given level.
 }
 
 function _setLoggingMode(){
